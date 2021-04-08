16189deafde4cb7926eae3a2a522dd208e803bac
clement.leconte <yoclement.leconte@epita.fr>
2021-04-09 01:08:59 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Cc c moi </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
