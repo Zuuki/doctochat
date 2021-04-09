a7b4750a91baf83f8cace5a4f64fd09284225e9e
clement.leconte <yoclement.leconte@epita.fr>
2021-04-09 13:52:06 +0200

    Failed last commit, didn't add anything else

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
+import React, { useRef, useState } from 'react';
 import './App.css';
 
-
+var userMessages = [];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p> Cc c moi </p>
-      </header>
+      <section>
+        <ChatRoom />
+      </section>
     </div>
   );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  //var userMessages = [];
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await userMessages.push({
+      text: formValue
+    })
+    
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {userMessages && userMessages.map(msg => <ChatMessage message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+function ChatMessage(props) {
+  const { text } = props.message;
+
+  return (<>
+    <div className={`message bot`}>
+      <img src={'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
 }
 
 export default App;
