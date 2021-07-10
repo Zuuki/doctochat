02bde4fcba9d25ca67fd620dd8d7319a129e494c
guccitaps <arthur.ulpat@epita.fr>
2021-07-10 14:31:01 +0200

    Update ics file generator

--- src/Bot.js
+++ src/Bot.js
@@ -1,40 +1,22 @@
 import { tree_answer, init_tree } from './decision_tree.js';
 
-function GenerateIcs(doctor) {
+function GenerateIcs(desc, time) {
+  //const ics = require('./dist');
+  let ics = require("ics")
+  let events = [];
+  for (let i = 0; i < desc.length; i++) {
+  	let event = {
+  	start: [time[i].substring(0, 4), time[i].substring(4, 6), time[i].substring(6, 8), time[i].substring(8, 10), time[i].substring(10, 12)],
+  	duration: { minutes: 30 },
+  	title: desc[i]
+  	};
+  	events.push(event);
+  }
+
+  const { error, value } = ics.createEvents(events);
   var element = document.createElement('a');
-  element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent('BEGIN:VCALENDAR\n' +
-'VERSION:2.0\n' +
-'CALSCALE:GREGORIAN\n' +
-'BEGIN:VEVENT\n' +
-'SUMMARY:rendez-vous avec ' + doctor +
-'\nDTSTART;TZID=America/New_York:20130802T103400\n' +
-'DTEND;TZID=America/New_York:20130802T110400\n' +
-'LOCATION:1000 Broadway Ave., Brooklyn\n' +
-'DESCRIPTION: Access-A-Ride to 900 Jay St., Brooklyn\n' +
-'STATUS:CONFIRMED\n' +
-'SEQUENCE:3\n' +
-'BEGIN:VALARM\n' +
-'TRIGGER:-PT10M\n' +
-'DESCRIPTION:Pickup Reminder\n' +
-'ACTION:DISPLAY\n' +
-'END:VALARM\n' +
-'END:VEVENT\n' +
-'BEGIN:VEVENT\n' +
-'SUMMARY:Access-A-Ride Pickup\n' +
-'DTSTART;TZID=America/New_York:20130802T200000\n' +
-'DTEND;TZID=America/New_York:20130802T203000\n' +
-'LOCATION:900 Jay St., Brooklyn\n' +
-'DESCRIPTION: Access-A-Ride to 1000 Broadway Ave., Brooklyn\n' +
-'STATUS:CONFIRMED\n' +
-'SEQUENCE:3\n' +
-'BEGIN:VALARM\n' +
-'TRIGGER:-PT10M\n' +
-'DESCRIPTION:Pickup Reminder\n' +
-'ACTION:DISPLAY\n' +
-'END:VALARM\n' +
-'END:VEVENT\n' +
-'END:VCALENDAR'));
-  element.setAttribute('download', 'event.ics');
+  element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(value));
+  element.setAttribute('download', 'doctolibrdv.ics');
   document.body.appendChild(element);
   element.click();
   console.log('tests');
@@ -60,6 +42,11 @@ export function bot_answer(text)
         if (text === "test")
         {
             return ["works"];
+        }
+        if (text === "1")
+        {
+        	GenerateIcs(["rdv docto 1", "rdv docto 2"], ["202201151430", "202201160915"]);
+        	return ["Voici votre fichier ics contenant vos rendez-vous"];
         }
         return ["not test"];
     } else if (context.mode === "end") {
